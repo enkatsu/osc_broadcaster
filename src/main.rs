e6dcd565ea91f6d24f987d1f701a671993a990e3
Katsuya Endoh <studio@enkatsu.org>
2023-04-17 15:34:25 +0900

    refactor socket

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,25 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddrV4, UdpSocket};
 use std::str::FromStr;
 
 const DEFAULT_IP_ADDRESS: &str = "127.0.0.1";
-const DEFAULT_PORT: i32 = 32000;
-
+const DEFAULT_PORT: u16 = 32000;
+const DEFAULT_SEND_PORT: u16 = 12000;
 
 struct BroadCaster {
     send_addresses: Vec<SocketAddrV4>,
-    send_port: i32,
-    socket: UdpSocket,
+    send_port: u16,
+    socket: Option<UdpSocket>,
     listen_ip_address: String,
-    listen_port: i32,
+    listen_port: u16,
 }
 
 impl Default for BroadCaster {
     fn default() -> Self {
-        let listen_ip_address = "127.0.0.1";
-        let listen_port = 32000;
-        let listen_address = SocketAddrV4::from_str(
-            &format!("{}:{}", listen_ip_address, listen_port).to_string()
-        ).unwrap();
         BroadCaster {
             send_addresses: Vec::new(),
-            send_port: 12000,
-            socket: UdpSocket::bind(listen_address).unwrap(),
-            listen_ip_address: listen_ip_address.to_string(),
-            listen_port,
+            send_port: DEFAULT_SEND_PORT,
+            socket: None,
+            listen_ip_address: DEFAULT_IP_ADDRESS.to_string(),
+            listen_port: DEFAULT_PORT,
         }
     }
 }
@@ -39,6 +34,14 @@ impl BroadCaster {
         return Self {
             ..Default::default()
         }
+    }
+
+    fn set_listen_ip_address(&mut self, listen_ip_address: String) {
+        self.listen_ip_address = listen_ip_address;
+    }
+
+    fn set_listen_port(&mut self, listen_port: u16) {
+        self.listen_port = listen_port;
     }
 
     fn push_send_address(&mut self, ip_address: String) -> bool {
@@ -70,12 +73,17 @@ impl BroadCaster {
             &format!("{}:{}", self.listen_ip_address, self.listen_port).to_string()
         ).unwrap();
 
-        println!("broadcast server: {}", self.socket.local_addr().unwrap().to_string());
+        let listen_address = SocketAddrV4::new(
+            Ipv4Addr::from_str(&self.listen_ip_address).unwrap(),
+            self.listen_port
+        );
+        self.socket = Some(UdpSocket::bind(listen_address).unwrap());
+        println!("broadcast server: {}:{}", self.listen_ip_address, self.listen_port);
         println!("send port: {}", self.send_port);
 
         let mut buf = [0u8; rosc::decoder::MTU];
         loop {
-            match self.socket.recv_from(&mut buf) {
+            match self.socket.as_ref().unwrap().recv_from(&mut buf) {
                 Ok((size, address)) => {
                     println!("Received packet with size {} from: {}", size, address);
                     let (_, packet) = rosc::decoder::decode_udp(&buf[..size]).unwrap();
@@ -107,7 +115,7 @@ impl BroadCaster {
                 } else {
                     let msg_buf = encoder::encode(&packet).unwrap();
                     for address in &self.send_addresses {
-                        self.socket.send_to(&msg_buf, address).unwrap();
+                        self.socket.as_ref().unwrap().send_to(&msg_buf, address).unwrap();
                     }
                     println!("OSC address: {}", msg.addr);
                     println!("OSC arguments: {:?}", msg.args);
@@ -117,7 +125,7 @@ impl BroadCaster {
                 for packet in &bundle.content {
                     let msg_buf = encoder::encode(&packet).unwrap();
                     for address in &self.send_addresses {
-                        self.socket.send_to(&msg_buf, address).unwrap();
+                        self.socket.as_ref().unwrap().send_to(&msg_buf, address).unwrap();
                     }
                 }
                 println!("OSC Bundle: {:?}", bundle);
@@ -126,7 +134,13 @@ impl BroadCaster {
     }
 }
 
+fn get_address_from_arg(arg: &str) -> SocketAddrV4 {
+    SocketAddrV4::from_str(arg).unwrap()
+}
+
 fn main() {
     let mut broad_caster = BroadCaster::new();
+    broad_caster.set_listen_ip_address("127.0.0.1".to_string());
+    broad_caster.set_listen_port(32000);
     broad_caster.start();
 }
